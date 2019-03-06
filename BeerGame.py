2c6d6ee3e4d6b33d5cf59c4442e0ffae35102a62
Konrad Miziolek <5964679+konradmiz@users.noreply.github.com>
2019-03-05 21:13:02 -0800

    Add files via upload

--- BeerGame.py
+++ BeerGame.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar  4 20:28:01 2019
 
-@author: Konrad
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -30,7 +25,6 @@ class Retailer(Business):
         Business.__init__(self, inventory)
     
     def create_order(self, customer_demand):
-        
         if customer_demand > 1.5 * self.inventory:
             return 3 * np.abs(self.inventory - customer_demand)
         elif (customer_demand <= 1.5 * self.inventory) and (customer_demand >= self.inventory / 1.5):
@@ -56,18 +50,20 @@ class Brewery(Business):
         Business.__init__(self, inventory)
     
     def brew_beer(self, wholesaler_demand):
-        pass
-
+        if wholesaler_demand > 1.5 * self.inventory:
+            return 3 * np.abs(self.inventory - wholesaler_demand)
+        elif (wholesaler_demand <= 1.5 * self.inventory) and (wholesaler_demand >= self.inventory / 1.5):
+            return wholesaler_demand
+        else:
+            return wholesaler_demand / 1.5
+    
+    
 DELAY = 4
 NUM_WEEKS = 24
 
 class Environment:
     def __init__(self):
-        self.a_store = Retailer(12)
-        self.a_wholesaler = Wholesaler(12)
-        self.a_brewer = Brewery(12)
-                 
-    def run_step(self):
+        
         self.brewer_inventory = [12, 12, 12, 12]
         self.wholesaler_inventory = [12, 12, 12, 12]
         self.retailer_inventory = [12, 12, 12, 12]
@@ -81,11 +77,15 @@ class Environment:
         self.wholesaler_sold = [4, 4, 4, 4]
         self.brewer_sold = [4, 4, 4, 4]
         
+        self.a_store = Retailer(self.brewer_inventory[-1])
+        self.a_wholesaler = Wholesaler(self.wholesaler_inventory[-1])
+        self.a_brewer = Brewery(self.retailer_inventory[-1])
+        
+    def run_simulation(self):        
         for i in range(DELAY, NUM_WEEKS):
             self.customer_order.append(8)
-            print(i)
 
-            self.a_brewer.get_goods(self.wholesaler_order[i])
+            self.a_brewer.get_goods(self.brewer_order[i-DELAY])
             delivered_to_wholesaler = self.a_brewer.sell_goods(self.wholesaler_order[i-DELAY])
             
             self.a_wholesaler.get_goods(delivered_to_wholesaler)
@@ -94,20 +94,20 @@ class Environment:
             self.a_store.get_goods(delivered_to_retailer)
             delivered_to_customer = self.a_store.sell_goods(self.customer_order[i])
             
-            self.wholesaler_sold.append(delivered_to_retailer)
             self.brewer_sold.append(delivered_to_wholesaler)
+            self.wholesaler_sold.append(delivered_to_retailer)
             self.retailer_sold.append(delivered_to_customer)
             
+            self.brewer_inventory.append(self.a_brewer.inventory)
             self.wholesaler_inventory.append(self.a_wholesaler.inventory)
             self.retailer_inventory.append(self.a_store.inventory)
-            self.brewer_inventory.append(self.a_brewer.inventory)
 
-            self.retailer_order.append(self.a_store.create_order(self.customer_order[i]))
-            self.wholesaler_order.append(self.a_wholesaler.create_order(self.retailer_order[i]))
             self.brewer_order.append(self.a_brewer.brew_beer(self.wholesaler_order[i]))
+            self.wholesaler_order.append(self.a_wholesaler.create_order(self.retailer_order[i]))
+            self.retailer_order.append(self.a_store.create_order(self.customer_order[i]))
 
 a_env = Environment()
-a_env.run_step()  
+a_env.run_simulation()  
 
 a_env.retailer_order
 a_env.retailer_inventory
@@ -115,13 +115,15 @@ a_env.retailer_inventory
 a_env.wholesaler_order
 a_env.wholesaler_inventory
 
+a_env.brewer_order
+a_env.brewer_inventory
 
-plt.plot(list(range(NUM_WEEKS)), a_env.retailer_order)
-plt.plot(list(range(NUM_WEEKS)), a_env.wholesaler_order)
-plt.plot(list(range()))
+model_time = list(range(NUM_WEEKS))
 
-#steady_state = np.repeat(4, DELAY)
-#week_1 = np.array([4])
-#remaining_weeks = np.repeat(8, NUM_WEEKS - 1)
-#
-#customer_demand = np.concatenate((steady_state, week_1, remaining_weeks))
+plt.plot(model_time, a_env.retailer_order)
+plt.plot(model_time, a_env.wholesaler_order)
+plt.plot(model_time, a_env.brewer_order)
+
+plt.plot(model_time, a_env.retailer_inventory)
+plt.plot(model_time, a_env.wholesaler_inventory)
+plt.plot(model_time, a_env.brewer_inventory)
